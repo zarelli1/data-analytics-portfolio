31fe0ab9b1b84163c7024d5897a4061afd4b576c
leonardo zarelli <143124804+zarelli1@users.noreply.github.com>
2026-06-01 20:54:15 -0300

    Update FilaPrioridade.sol

--- evm-data-structure/priority-queue/FilaPrioridade.sol
+++ evm-data-structure/priority-queue/FilaPrioridade.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
@@ -6,21 +5,25 @@ contract FilaPrioridade {
 
     struct No {
         uint256 prioridade;
+        uint256 chegada;
         string  dado;
     }
 
     No[]    private pilha;
     uint256 private tamanho;
+    uint256 private contador;
 
     function enqueue(uint256 p, string calldata d) external {
-        if (tamanho == pilha.length) pilha.push(No(p, d));
-        else pilha[tamanho] = No(p, d);
+        No memory novo = No(p, contador++, d);
+
+        if (tamanho == pilha.length) pilha.push(novo);
+        else pilha[tamanho] = novo;
 
         uint256 i = tamanho++;
 
         while (i > 0) {
             uint256 pai = (i - 1) / 2;
-            if (pilha[i].prioridade < pilha[pai].prioridade) {
+            if (_menor(i, pai)) {
                 _swap(i, pai);
                 i = pai;
             } else break;
@@ -30,7 +33,7 @@ contract FilaPrioridade {
     function dequeue() external returns (uint256, string memory) {
         require(tamanho > 0, "fila vazia");
 
-        uint256 p      = pilha[0].prioridade;
+        uint256       p = pilha[0].prioridade;
         string memory d = pilha[0].dado;
 
         pilha[0] = pilha[--tamanho];
@@ -38,8 +41,8 @@ contract FilaPrioridade {
         uint256 i = 0;
         while (true) {
             uint256 e = 2*i+1, d2 = 2*i+2, m = i;
-            if (e  < tamanho && pilha[e ].prioridade < pilha[m].prioridade) m = e;
-            if (d2 < tamanho && pilha[d2].prioridade < pilha[m].prioridade) m = d2;
+            if (e  < tamanho && _menor(e,  m)) m = e;
+            if (d2 < tamanho && _menor(d2, m)) m = d2;
             if (m != i) { _swap(i, m); i = m; } else break;
         }
 
@@ -53,6 +56,13 @@ contract FilaPrioridade {
 
     function size() external view returns (uint256) {
         return tamanho;
+    }
+
+    // retorna true se o nó em `a` deve sair antes do nó em `b`
+    function _menor(uint256 a, uint256 b) private view returns (bool) {
+        if (pilha[a].prioridade != pilha[b].prioridade)
+            return pilha[a].prioridade < pilha[b].prioridade;
+        return pilha[a].chegada < pilha[b].chegada;
     }
 
     function _swap(uint256 a, uint256 b) private {
