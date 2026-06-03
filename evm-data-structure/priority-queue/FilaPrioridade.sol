e588731eab23d73cae6af6d1614e150172dd9a52
leonardo zarelli <143124804+zarelli1@users.noreply.github.com>
2026-06-03 19:54:30 -0300

    Update FilaPrioridade.sol

--- evm-data-structure/priority-queue/FilaPrioridade.sol
+++ evm-data-structure/priority-queue/FilaPrioridade.sol
@@ -1,73 +1,61 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
 contract FilaPrioridade {
 
-    struct No {
-        uint256 prioridade;
+    struct Cliente {
+        address endereco;
+        uint8 gravidade;
         uint256 chegada;
-        string  dado;
     }
 
-    No[]    private pilha;
-    uint256 private tamanho;
+    Cliente[] public fila;
     uint256 private contador;
 
-    function enqueue(uint256 p, string calldata d) external {
-        No memory novo = No(p, contador++, d);
+    function entrar(uint8 _gravidade) external {
+        require(_gravidade >= 1 && _gravidade <= 10, "Gravidade invalida");
+        fila.push(Cliente(msg.sender, _gravidade, contador));
+        contador++;
+        _ordenar();
+    }
 
-        if (tamanho == pilha.length) pilha.push(novo);
-        else pilha[tamanho] = novo;
+    function atender() external returns (address) {
+        require(fila.length > 0, "Fila vazia");
+        address atendido = fila[0].endereco;
+        for (uint256 i = 0; i < fila.length - 1; i++) {
+            fila[i] = fila[i + 1];
+        }
+        fila.pop();
+        return atendido;
+    }
 
-        uint256 i = tamanho++;
-
+    function _ordenar() internal {
+        uint256 i = fila.length - 1;
         while (i > 0) {
-            uint256 pai = (i - 1) / 2;
-            if (_menor(i, pai)) {
-                _swap(i, pai);
-                i = pai;
-            } else break;
+            uint256 j = i - 1;
+            if (_temPrioridade(fila[i], fila[j])) {
+                Cliente memory temp = fila[i];
+                fila[i] = fila[j];
+                fila[j] = temp;
+                i = j;
+            } else {
+                break;
+            }
         }
     }
 
-    function dequeue() external returns (uint256, string memory) {
-        require(tamanho > 0, "fila vazia");
-
-        uint256       p = pilha[0].prioridade;
-        string memory d = pilha[0].dado;
-
-        pilha[0] = pilha[--tamanho];
-
-        uint256 i = 0;
-        while (true) {
-            uint256 e = 2*i+1, d2 = 2*i+2, m = i;
-            if (e  < tamanho && _menor(e,  m)) m = e;
-            if (d2 < tamanho && _menor(d2, m)) m = d2;
-            if (m != i) { _swap(i, m); i = m; } else break;
+    function _temPrioridade(Cliente memory a, Cliente memory b) internal pure returns (bool) {
+        if (a.gravidade != b.gravidade) {
+            return a.gravidade > b.gravidade;
         }
-
-        return (p, d);
+        return a.chegada < b.chegada;
     }
 
-    function peek() external view returns (uint256, string memory) {
-        require(tamanho > 0, "fila vazia");
-        return (pilha[0].prioridade, pilha[0].dado);
+    function verFila() external view returns (Cliente[] memory) {
+        return fila;
     }
 
-    function size() external view returns (uint256) {
-        return tamanho;
-    }
-
-    // retorna true se o nó em `a` deve sair antes do nó em `b`
-    function _menor(uint256 a, uint256 b) private view returns (bool) {
-        if (pilha[a].prioridade != pilha[b].prioridade)
-            return pilha[a].prioridade < pilha[b].prioridade;
-        return pilha[a].chegada < pilha[b].chegada;
-    }
-
-    function _swap(uint256 a, uint256 b) private {
-        No memory t = pilha[a];
-        pilha[a]    = pilha[b];
-        pilha[b]    = t;
+    function tamanho() external view returns (uint256) {
+        return fila.length;
     }
 }
